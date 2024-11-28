5b8372ab7c45f6e83828fcee194ef80a3c9eaee1
tpra0011 <tpra0011@student.monash.edu>
2024-11-28 09:47:18 +0800

    Completed unicafe exercises

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,45 +6,98 @@ const Button = ({eventHandler, type}) => {
   )
 }
 
+const StatisticLine = ({text, value, unit}) => {
+
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value} {unit}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad, all, avg, positive}) => {
+  if(good === 0 && bad === 0 && neutral == 0 ) {
+    return (
+      <p>
+        No Feedback Given
+      </p>
+    )
+  } else {
+  return(
+    <div>
+      <h2>
+        statistics
+      </h2>
+      <table>
+        <tbody>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={all}/>
+        <StatisticLine text="average" value={avg}/>
+        <StatisticLine text="positive" value={positive} unit='%'/>
+        </tbody>
+      </table>
+    </div>
+    )
+  } 
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0)
+
 
   const handleButton = ({type}) => {
+      let newGood = good
       if(type == 'good'){
-        const newGood = good + 1
+        newGood = good + 1
         setGood(newGood)
+        
       }
-
+      let newNeutral = neutral
       if(type == 'neutral'){
-        const newNeutral = neutral + 1
+        newNeutral = neutral + 1
         setNeutral(newNeutral)
+        
       }
+      let newBad = bad
       if(type == 'bad'){
-        const newBad = bad + 1
+        newBad = bad + 1
         setBad(newBad)
+        
       }
+      const newAll = all + 1
+      // console.log(newAll)
+      setAll(newAll)
+
+      const newAvg = (newGood*1 + newBad*(-1))/newAll
+      setAvg(newAvg)
+
+      const newPositive = (newGood/newAll)*100
+      setPositive(newPositive)
   }
   
   return (
+
     <div>
       <h1>
         give feedback
       </h1>
-     <Button eventHandler={handleButton} type='good'/>
-     <Button eventHandler={handleButton} type='neutral'/>
-     <Button eventHandler={handleButton} type='bad'/>
-      <h2>
-        statistics
-      </h2>
-      <span>good {good}</span><br/>
-      <span>neutral {neutral}</span><br/>
-      <span>bad {bad}</span>
-
+      <Button eventHandler={handleButton} type='good'/>
+      <Button eventHandler={handleButton} type='neutral'/>
+      <Button eventHandler={handleButton} type='bad'/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} avg={avg} positive={positive}/>
 
     </div>
   )
+
 }
 
 export default App
